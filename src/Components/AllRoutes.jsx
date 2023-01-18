8035c3121af6cc2861ae7c424f67201f1e902ca9
rahulyadav826870 <rahulyadav826870@gmail.com>
2023-01-19 00:00:42 +0530

    done day2 work

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import All_Sarees from '../Pages/All_Sarees';
-import All_Top_Wear from '../Pages/All_Top_Wear';
-import Bed_Sheets from '../Pages/Bed_Sheets';
-import Home from '../Pages/Home';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import All_Sarees from "../Pages/All_Sarees";
+import All_Top_Wear from "../Pages/All_Top_Wear";
+import Bed_Sheets from "../Pages/Bed_Sheets";
+import Home from "../Pages/Home";
+import SingleProductPage from "../Routes/SingleProductPage";
+
+import { singleProductName } from "../All Data/singleProductName";
 
 const AllRoutes = () => {
   return (
     <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/All Sarees' element={<All_Sarees/>}/>
-    <Route path='/All Top Wear' element={<All_Top_Wear/>}/>
-    <Route path='/Bed Sheets' element={<Bed_Sheets/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/All Sarees" element={<All_Sarees />} />
+      {singleProductName.map((el, ind) => (
+        <Route key={ind} path={`/${el}/:id`} element={<SingleProductPage />} />
+      ))}
+     
+      <Route path="/All Top Wear" element={<All_Top_Wear />} />
+      <Route path="/Bed Sheets" element={<Bed_Sheets />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
