daee1e9da97701fb0b0d460a2db253704cb8d408
MD Sakil Ahmed <sakilahmed965@gmail.com>
2023-01-18 09:22:07 +0530

    saree list

---
+++ src/Components/Saree/SareeList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from "axios"
+import { useEffect } from 'react'
+import SareeCard from './SareeCard'
+import styles from "../Saree/Styles/SareeList.module.css"
+
+
+const SareeList = () => {
+    const [sareeData,setSareeData]=useState([])
+   
+    const getSarrees=()=>{
+        axios.get("https://product-list-api.onrender.com/saree").then((res)=>setSareeData(res.data)).catch((err)=>console.log(err))
+    }
+useEffect(()=>{
+getSarrees()
+},[])
+
+console.log(sareeData);
+  return (
+    <div className={styles.sareeList}>
+        {
+           
+           sareeData && sareeData.map((ele)=>{
+               return <SareeCard key={ele.id} {...ele}/>
+            })
+            
+        }
+    </div>
+  )
+}
+
+export default SareeList
---
+++ src/Components/Saree/SidebarSaree.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarSaree = () => {
+  return (
+    <div>SidebarSaree</div>
+  )
+}
+
+export default SidebarSaree
