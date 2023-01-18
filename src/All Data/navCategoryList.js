16e135a2d1972e73d152a63dd6645408dd59ae48
santosh <styadav018@gmail.com>
2023-01-18 15:20:46 +0530

    added navbar only

---
+++ src/All Data/navCategoryList.js
@@ -0,0 +1,265 @@
+export  const navListData = {
+    "Women Ethnic":[
+      {
+        category: 'All Women Ethnic',
+        sub_category: ['View All'],
+      },
+      {
+        category: 'Sarees',
+        sub_category: [
+          'All Sarees',
+          'Silk Sarees',
+          'Cotton Silk Sarees',
+          'Cotton Sarees',
+          'Georgette Sarees',
+          'Chiffon Sarees',
+          'Satin Sarees',
+          'Embroidered Sarees',
+        ],
+      },
+      {
+        category: 'Kurtis',
+        sub_category: [
+          'All Kurtis',
+          'Anarkali',
+          'Kurties',
+          'Rayon',
+          'Kurties',
+          'Cotton',
+          'Kurties',
+          'Embroidered',
+          'Kurtis',
+        ],
+      },
+      {
+        category: 'Kurta Sets',
+        sub_category: ['All Kurtis Sets'],
+      },
+      {
+        category: 'Suits & Dress Material',
+        sub_category: [
+          'All Suits & Dress Material',
+          'Cotton',
+          'Embroidered Suits',
+          'Chanderi Suits',
+        ],
+      },
+      {
+        category: 'Other Ethnic',
+        sub_category: [
+          'Blouses',
+          'Dupattas',
+          'Lehanga',
+          'Gown',
+          'Ethnic Bottomwear',
+        ],
+      },
+    ],
+    "Women Western":[
+      {
+        category: 'Bottomwear',
+        sub_category: ['Jeans', "Jegging", "Palazzos", "Shorts" ,  "Skirts"],
+      },
+      {
+        category: 'Innerwear',
+        sub_category: ['Bra', "Brief"],
+      },
+      {
+        category: 'Sleepwear',
+        sub_category: ['Nightsuits', "Babydolls" ],
+      },
+    ],
+    "Men" : [
+      {
+        category: 'Top Wear',
+        sub_category: ['All Top Wear', "Tshirts", "Shirts", "Track Pants" ,  "Skirts"],
+      },
+      {
+        category: 'Bottom Wear',
+        sub_category: [ "Track Pants" ,  "Jeans" , "Trousers" ],
+      },
+      {
+        category: 'Men Assessories',
+        sub_category: [
+          'All Men Accessories', "Watches", "Belts", "Wallet" ,  
+          "Jewellery",
+          "Sunglasses",
+          "Bags",
+        ],
+      },
+      {
+        category: 'Men Footwear',
+        sub_category: [
+          'Casual Shoe', "Sports Shoe", "Sandals", "Formal Shoes" 
+        ],
+      },
+      {
+        category: 'Ethnic Wear',
+        sub_category: [
+          'Men Kurtas', "Ethnic Jackets", 
+        ],
+      },
+      {
+        category: 'Inner & Sleep Wear',
+        sub_category: [
+          'All Inner & Sleep', "Vests", 
+        ],
+      },
+    ],
+    "Kids":[
+      {
+        category: 'Boy & Girls 2+ Years',
+        sub_category: [
+          'Dresses', 
+        ],
+      },
+      {
+        category: 'Infant 0-2 Years',
+        sub_category: [
+          'Rompers', 
+        ],
+      },
+      {
+        category: 'Toys & Accessories',
+        sub_category: [
+          'Soft Toys',
+          'Footwear',
+          'Stationery',
+          'Wtaches',
+          'Bags & Backpacks',
+          'All Baby Care'
+        ],
+      },
+    ],
+    "Home & Kitchen" : [
+      {
+        category: 'Home Furnishing',
+        sub_category: [
+          'BedSheets',
+          'Doormats',
+          'Curtains & Sheers',
+          'Cushion & Cusion Covers',
+          'Mattress Protectors' 
+        ],
+      },
+      {
+        category: 'Home Decor',
+        sub_category: [
+          'All Home Decor',
+          'Stickers',
+          'Clocks',
+          'Showpieces', 
+        ],
+      },
+      {
+        category: 'Kitchen & Dining',
+        sub_category: [
+          'Kitchen Storage',
+          'Cookware & Bakeware',
+        ],
+      },
+    ],
+    "Beauty & Health" : [
+      {
+        category: 'Make up',
+        sub_category: [
+          'Face',
+          'Eyes',
+          'Lips',
+          'Nails',
+        ],
+      },
+      {
+        category: 'Wellness',
+        sub_category: [
+          'Sanitizers',
+          'Oral Care',
+          'Feminine Hygiene',
+        ],
+      },
+      {
+        category: 'Skincare',
+        sub_category: [
+          'Deodorants',
+        ],
+      },
+    ],
+    "Jewellery & Accessories" : [
+      {
+        category: 'Jewellery',
+        sub_category: [
+          'Jewellery Set',
+          'Earrings',
+          'Mangalsutras',
+          'Studs',
+          'Bangles',
+          'Necklaces',
+          'Rings',
+          'Anklets' 
+        ],
+      },
+      {
+        category: 'Women Accessory',
+        sub_category: [
+          'Bags',
+          'Watches',
+          'Hair Accessories',
+          'Sunglasses',
+        ],
+      },
+    ],
+    "Bags & Footwear" : [
+      {
+        category: 'Women Bags',
+        sub_category: [
+          'All Women Bags',
+           "Handbags", 
+           'Clutches',
+           'Slingbags'
+        ],
+      },
+      {
+        category: 'Men Bags',
+        sub_category: [
+          'All Men Bags',
+          'Men Wallets'
+        ],
+      },
+      {
+        category: 'Men Footwear',
+        sub_category: [
+          'Sports Shoes',
+          'Casual Shoes',
+          'Formal Shoes',
+          'Sandals'
+        ],
+      },
+      {
+        category: 'Women Footwear',
+        sub_category: [
+          'Flats',
+          'Bellies',
+          'Juttis',
+        ],
+      },
+    ],
+    "Electronics" : [
+      {
+        category: 'Mobile & Accessories',
+        sub_category: [
+          'All Mobile & Accessories' ,
+          'Smartwatches',
+          'Mobile Holders',
+          'Mobile cases and covers'
+        ],
+      },
+      {
+        category: 'Application',
+        sub_category: [
+          'All Application' ,
+          'Grooming',
+          'Home Appliances'
+        ],
+      },
+    ]
+  }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='h-[900vh]'>Home</div>
   )
 }
 
