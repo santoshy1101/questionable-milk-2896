d60d1310d69dabe3cb31d93a00482745c1610d30
rahulyadav826870 <rahulyadav826870@gmail.com>
2023-01-22 12:49:51 +0530

    empty cart css

--- src/Pages/AddToCart/AddToCart.jsx
+++ src/Pages/AddToCart/AddToCart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import './AddToCart.css'
+import "./AddToCart.css";
 import {
   decrementActionCart,
   getTotalActionCart,
